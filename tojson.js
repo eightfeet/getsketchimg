5eb56a580a6e579646906a52a3aa34a514b57391
eightfeet <bajiao000000@163.com>
2018-11-17 09:25:29 +0800

    update

--- tojson.js
+++ tojson.js
@@ -5,9 +5,9 @@ var start = null;
 var end = null;
 
 var arg = (process.argv[2] && process.argv[2].split('-')) || 1;
-var mdid = parseInt(arg[1], 10) || null;
+var mdid = parseInt(arg[0], 10) || null;
 
-var male = null;
+var male = false;
 var isBody = true;
 
 var fileDirectory = './renameimages';
@@ -20,11 +20,11 @@ var item = function(isy, img){
         "isClothes": isBody !== true ? true : false,
         "isBody": isBody === true ? true : false,
         "isMale": false,
-        "isFemale": false,
+        "isFemale": true,
         "isHeader": false,
         "isHandsFeet": false,
         "isHalf": false,
-        "isGroup": true,
+        "isGroup": false,
         "mdId": `md${mdid}`,
         "selected": false
     }
@@ -36,8 +36,16 @@ var end = null;
 var files = fs.readdirSync(fileDirectory);
 
 for (var index = 0; index < files.length; index++) {
+    console.log(555, files[index]);
+    if (files[index] === '.DS_Store') {
+        continue;
+    }
     var element = files[index].split('.')[0].split('-')[1].split('&')[0];
+    
     if (index === 0) {
+        start = files[index].split('-')[0];
+    }
+    if (index === 1 && files[0] === '.DS_Store') {
         start = files[index].split('-')[0];
     }
     if (index === files.length - 1) {
@@ -47,5 +55,8 @@ for (var index = 0; index < files.length; index++) {
 }
 
 fs.writeFileSync(`models${start}_${end}.json`, JSON.stringify(data));
+
+fs.writeFileSync(`./../sketch-2018/src/data/models${start}_${end}.json`, JSON.stringify(data));
+
 
 console.log(`${male ? '男' : '女'}模特 models${start}_${end}.json is compiled`)
